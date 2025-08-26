b84a435ad6b8e23bb628bee20c09b21a76fa835c
Jagmeet Singh <jagmeetsingh2921@gmail.com>
2025-08-26 14:10:22 +0530

    format specifiers

--- bro-code/02-format-specifiers.c
+++ bro-code/02-format-specifiers.c
@@ -12,11 +12,47 @@ int main(){
     char currency = '$';
     char name[] = "Jagmeet singh";
 
-    printf("%d\n", age);
-    printf("%f\n", price);
-    printf("%lf\n", pi);
-    printf("%c\n", currency);
-    printf("%s\n", name);
+    // printf("%d\n", age);
+    // printf("%f\n", price);
+    // printf("%lf\n", pi);
+    // printf("%c\n", currency);
+    // printf("%s\n", name);
+
+
+    int num1 = 1;
+    int num2 = 10;
+    int num3 = 100;
+
+    // printf("%d\n", num1);
+    // printf("%d\n", num2);
+    // printf("%d\n", num3);
+
+    // printf("%04d\n", num1);
+    // printf("%04d\n", num2);
+    // printf("%04d\n", num3);
+
+    // num3 = -100;
+    
+    // printf("%+d\n", num1);
+    // printf("%+d\n", num2);
+    // printf("%+d\n", num3);
+
+
+    float price1 = 19.99;
+    float price2 = 1.50;
+    float price3 = -100.00;
+
+
+    printf("%.4f\n", price1);
+    printf("%.2f\n", price2);
+    printf("%.2f\n", price3);
+
+    // width precision flags
+    printf("%7.4f\n", price1);
+    printf("%7.4f\n", price2);
+    printf("%7.4f\n", price3);
+
+
 
     return 0;
 }
