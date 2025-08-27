8f311586c01f25aa13659ebe5f1e6914dff28f51
Jagmeet Singh <jagmeetsingh2921@gmail.com>
2025-08-27 14:59:05 +0530

    arithematic operations

---
+++ bro-code/03-arth-op.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main(){
+    int x = 10;
+    int y = 3;
+    float z = 0;
+
+    // z = x + y;
+    // z = x - y;
+    // z = x * y;
+    // z = x / y;
+    // z = x % y;
+    // x+=3;
+    // x-=3;
+    // x+=3;
+    // x/=2;
+
+    printf("%d",x);
+
+    return 0;
+}
