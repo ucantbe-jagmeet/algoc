00b262a58b14b0398ef0b29ee7a278a33eb030aa
Jagmeet Singh <jagmeetsingh2921@gmail.com>
2025-08-29 16:06:42 +0530

    user input

--- bro-code/04-user-input.c
+++ bro-code/04-user-input.c
@@ -6,11 +6,21 @@ int main(){
     float gpa = 0.0f;
     char grade = '\0'; // null terminator character
     char name[30] = ""; 
+    
     printf("Enter your age: ");
     scanf("%d", &age);
 
+    printf("Enter your gpa: ");
+    scanf("%f", &gpa);
+
+    printf("Enter your grade: ");
+    scanf(" %c", &grade);
+
+    printf("Enter your FirstName: ");
+    scanf("%s", &name);
+
     printf("age is %d\n", age);
-    printf("gpa is %f\n", gpa);
+    printf("gpa is %.2f\n", gpa);
     printf("grade is %c\n", grade);
     printf("name is %s\n", name);
 
