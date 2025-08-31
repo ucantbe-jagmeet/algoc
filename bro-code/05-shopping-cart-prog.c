00c5a6ba4af16ff2479001f693484717de27b959
Jagmeet Singh <jagmeetsingh2921@gmail.com>
2025-08-31 18:16:53 +0530

    Shopping Cart prog

--- bro-code/05-shopping-cart-prog.c
+++ bro-code/05-shopping-cart-prog.c
@@ -3,5 +3,27 @@
 
 int main(){
 
+    char item[50]= "";
+    float price = 0.0f;
+    int quantity = 0;
+    char currency = '$';
+    float total= 0.0f;
+
+    printf("What item would you like to buy?: ");
+    fgets(item, sizeof(item), stdin);
+
+    item[strlen(item) -1] = '\0';
+
+    printf("What is the price for each?: ");
+    scanf("%f", &price);
+
+    printf("How many would you like?: ");
+    scanf("%d", &quantity);
+
+    total = price * quantity;
+
+    printf("You have bought %d %s for price: %c%.2f", quantity, item, currency, total);
+
+
     return 0;
 }
