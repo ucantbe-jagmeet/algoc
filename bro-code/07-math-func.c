4653cbf888b08a908d44947509387cf700388e56
Jagmeet Singh <jagmeetsingh2921@gmail.com>
2025-09-03 15:41:33 +0530

    math functions

---
+++ bro-code/07-math-func.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <math.h>
+
+int main(){
+    int x = 9; 
+    // x = sqrt(x);
+    // x = poq(x, 4);
+    // x = round(x);
+    // x = ceil(x);
+    // x = floor(x);
+    // x = abs(x);
+    x = log(x);
+
+
+    printf("%d", x);
+    return 0;
+}
