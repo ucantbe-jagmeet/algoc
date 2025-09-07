e4ff19cddb2c297bedfe4b038a801b3436df99a4
Jagmeet Singh <jagmeetsingh2921@gmail.com>
2025-09-07 17:27:13 +0530

    compound interest calculator

--- bro-code/09-compound-interest-cal.c
+++ bro-code/09-compound-interest-cal.c
@@ -2,6 +2,30 @@
 #include <math.h>
 
 int main(){
+   double principal = 0.0;
+    double rate = 0.0;
+    int years = 0;
+    int timesCompounded = 0;
+    double total = 0.0;
+
+    printf("Compound Interest Calculator\n");
+
+    printf("Enter the principal (P): ");
+    scanf("%lf", &principal);
+
+    printf("Enter the interest rate %% (r): ");
+    scanf("%lf", &rate);
+    rate = rate / 100;
+
+    printf("Enter the # of years (t): ");
+    scanf("%d", &years);
+
+    printf("Enter # of times compounded per year (n): ");
+    scanf("%d", &timesCompounded);
+
+    total = principal * pow(1 + rate / timesCompounded, timesCompounded * years);
+
+    printf("The total amount after %.2f years is: %.2lf\n", (double)years, total);
 
     return 0;
 }
