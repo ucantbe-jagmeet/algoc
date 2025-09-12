b67addf5f8f74b27748ebdc58c07489ca7260d16
Jagmeet Singh <jagmeetsingh2921@gmail.com>
2025-09-12 21:17:06 +0530

    function prototype

---
+++ bro-code/12-fun-prototype.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <math.h>
+
+int add(int x , int y); // function prototype 
+int sub(int x , int y); // function prototype 
+
+int main() {
+    int result = add(3,4);
+    int result2 = sub(10,4);
+
+    printf("%d\n", result);
+    printf("%d", result2);
+    return 0;
+}
+
+int add(int x , int y) {
+    int result = x + y;
+    return result;
+}
+
+int sub(int x , int y) {
+    int result2 = x - y;
+    return result2;
+}
