359bbdceca1e0949b35296ff78d1b9cea3d43346
Jagmeet Singh <jagmeetsingh2921@gmail.com>
2025-09-13 17:24:41 +0530

    nested loop

--- bro-code/15-nested-loops.c
+++ bro-code/15-nested-loops.c
@@ -1,8 +1,26 @@
-
 #include <stdio.h>
-#include <unistd.h>
 
-int main(){
+int main() {
+
+    int rows = 0;
+    int columns = 0;
+    char symbol = '\0';
+
+    printf("Enter the # of rows: ");
+    scanf("%d", &rows);
+
+    printf("Enter the # of columns: ");
+    scanf("%d", &columns);
+
+    printf("Enter a symbol to use: ");
+    scanf(" %c", &symbol);
+
+    for (int i = 0; i < rows; i++) {
+        for (int j = 0; j < columns; j++) {
+            printf("%c ", symbol);
+        }
+        printf("\n");
+    }
 
 
     return 0;
