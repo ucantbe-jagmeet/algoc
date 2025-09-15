7136a84eee5c926f124f586c2c165966d03ce328
Jagmeet Singh <jagmeetsingh2921@gmail.com>
2025-09-15 17:33:26 +0530

    random number

--- bro-code/16-random-no.c
+++ bro-code/16-random-no.c
@@ -1,6 +1,17 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
 
 int main() {
+    // printf("%d", rand());
 
+    srand(time(NULL));
+    printf("%d\n", rand());
+    
+    srand(time(NULL));
+    int random = (rand() % 10) + 1;
+
+    printf("random %ld", random);
+    
     return 0;
 }
