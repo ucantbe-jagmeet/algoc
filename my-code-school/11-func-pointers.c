eb5534bfaffad2643657b9824c923dd356c32825
Jagmeet Singh <jagmeetsingh2921@gmail.com>
2025-10-08 11:47:44 +0530

    funciton poitner char

--- my-code-school/11-func-pointers.c
+++ my-code-school/11-func-pointers.c
@@ -4,6 +4,10 @@ int Add( int a, int b ) {
     return a + b;
 }
 
+void Print( char* name ) {
+    printf("Hello, %s\n", name);
+}
+
 int main() {
     int c;
     int (*p)(int, int);
@@ -11,6 +15,10 @@ int main() {
     c = p(4, 5); // dereferencing and execution function pointer
     printf("The sum is: %d\n", c);
 
+
+    void (*str)(char*);
+    str = Print;
+    str("John Doe");
 
     return 0;
 }
