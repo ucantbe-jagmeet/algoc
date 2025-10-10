702041d07f4fb053dc4c93ed92ffd8e2053f8295
Jagmeet Singh <jagmeetsingh2921@gmail.com>
2025-10-10 11:59:42 +0530

    funciton poitner callback

--- my-code-school/11-func-pointers-callback.c
+++ my-code-school/11-func-pointers-callback.c
@@ -4,11 +4,11 @@ void A() {
     printf("Hello!");
 }
 
-void B(void (*ptr)()) {
-    ptr();
+void B(void (*ptr)()) { // function takes a function pointer as an argument
+    ptr(); // call back function that "ptr" points to
 }
 
 int main() {
-    B(A);
+    B(A); //   A is passed as a callback function to B
     return 0;
 }
