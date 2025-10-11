50055c641b74149d575139e2d67598a1fdd5d15d
Jagmeet Singh <jagmeetsingh2921@gmail.com>
2025-10-11 12:09:52 +0530

    bubble Sort callback exaple

--- my-code-school/13-bble-sort.c
+++ my-code-school/13-bble-sort.c
@@ -1,10 +1,15 @@
 #include <stdio.h>
 
-void BubbleSort(int Arr[], int size) {
+int compare(int a, int b) {
+    if( a > b ) return 1;
+    else return -1;
+}
+
+void BubbleSort(int Arr[], int size,int (*compare)(int, int)) {
     int i, j, temp;
     for (i = 0; i < size - 1; i++) {
         for (j = 0; j < size - i - 1; j++) {
-            if (Arr[j] > Arr[j + 1]) {
+            if (compare(Arr[j], Arr[j + 1]) > 0) {
                 temp = Arr[j];
                 Arr[j] = Arr[j + 1];
                 Arr[j + 1] = temp;
@@ -13,8 +18,9 @@ void BubbleSort(int Arr[], int size) {
     }
 }
 int main() {
-    int i, Arr[5] = {64, 34, 25, 12, 22};
-    BubbleSort(Arr, 5);
+    // int i, Arr[5] = {64, 34, 25, 12, 22};
+    int i, Arr[5] = {-64, 34, 25, -12, 22};
+    BubbleSort(Arr, 5, compare);
     for(int i = 0; i < 5; i++) printf("%d ", Arr[i]);
     return 0;
 }
