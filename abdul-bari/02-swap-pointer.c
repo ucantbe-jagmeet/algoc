1ed1e2b79ef58799c5fc1b6fed79c7ed7a8aee53
Jagmeet Singh <jagmeetsingh2921@gmail.com>
2025-10-12 16:56:25 +0530

    swapping numbers using pointers

---
+++ abdul-bari/02-swap-pointer.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+void swap (int* a, int* b){
+    int temp = *a;
+    *a = *b;
+    *b = temp;
+}
+
+
+int main(){
+
+    int a= 2 , b= 10;
+    swap(&a, &b);
+    printf("Numbers after swqpping are: %d and %d\n",a, b);
+    return 0;
+}
