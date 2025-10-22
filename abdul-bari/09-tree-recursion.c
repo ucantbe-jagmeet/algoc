80277d1d88f075d1a3ef0038d62d277eadec61ea
Jagmeet Singh <jagmeetsingh2921@gmail.com>
2025-10-22 14:25:58 +0530

    tree recurssion

---
+++ abdul-bari/09-tree-recursion.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int x = 0;
+
+int fun(int n){
+ 
+    if( n > 0){
+        printf("%d ", n);
+        fun(n-1);
+        fun(n-1);
+    }
+    return 0;
+}
+
+int main() {
+    int n =4;
+    fun(n);
+    return 0;
+}
