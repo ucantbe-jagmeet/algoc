1483b6004067a4a1931f98b45321afb919d86175
Jagmeet Singh <jagmeetsingh2921@gmail.com>
2025-12-12 21:14:18 +0530

    Scope variables

--- bro-code/10-functions.c
+++ bro-code/10-functions.c
@@ -1,14 +1,21 @@
 #include <stdio.h>
-
-int add(int x , int y); // function prototype
-
-int main() {
-    int result = add(3,4);
-    printf("%d", result);
-    return 0;
-}
+#include <math.h>
 
 int add(int x , int y) {
     int result = x + y;
     return result;
+}
+
+int sub(int x , int y) {
+    int result2 = x - y;
+    return result2;
+}
+
+int main() {
+    int result = add(3,4);
+    int result2 = sub(10,4);
+
+    printf("%d\n", result);
+    printf("%d", result2);
+    return 0;
 }
