004d2be9d16cf7fea14dc34857b89cf967f48f74
Jagmeet Singh <jagmeetsingh2921@gmail.com>
2025-12-12 21:20:41 +0530

    function prototype

--- bro-code/12-fun-prototype.c
+++ bro-code/12-fun-prototype.c
@@ -4,6 +4,12 @@
 int add(int x , int y); // function prototype 
 int sub(int x , int y); // function prototype 
 
+// fun prototype -> PRovide the compiler with information about a fuction's: name, return type and 
+                    // parameters befeore its actual definition. Enables type checking and allows functions to be 
+                    // usef before they are defined. 
+// It improves readability , organization and helps prevent errors.
+
+
 int main() {
     int result = add(3,4);
     int result2 = sub(10,4);
