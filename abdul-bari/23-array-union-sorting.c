55a4468a73337f2345047c9a40c6ecb58d6747d3
Jagmeet Singh <jagmeetsingh2921@gmail.com>
2026-04-17 16:41:43 +0530

    union sorting

---
+++ abdul-bari/23-array-union-sorting.c
@@ -0,0 +1,50 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+struct Array {
+    int A[20];
+    int size;
+    int length;
+};
+
+void DisplayNum(struct Array arr) {
+    printf("\nElements are: ");
+    for (int i = 0; i < arr.length; i++) {
+        printf("%d ", arr.A[i]);
+    }
+}
+void Union(struct Array *arr1, struct Array *arr2, struct Array *arr3){
+    int i = 0, j = 0, k = 0;
+
+    while (i < arr1->length && j < arr2->length) {
+        if (arr1->A[i] < arr2->A[j]) {
+            arr3->A[k++] = arr1->A[i++];
+        } else if(arr2->A[j] < arr1->A[i]) {
+            arr3->A[k++] = arr2->A[j++];
+        } else {
+            arr3->A[k++] = arr1->A[i++];
+            j++;
+        }
+    }
+
+    while (i < arr1->length) {
+        arr3->A[k++] = arr1->A[i++];
+    }
+
+    while (j < arr2->length) {
+        arr3->A[k++] = arr2->A[j++];
+    }
+
+    arr3->length = k;
+}
+
+int main() {
+    struct Array arr1 = {{2, 6, 10, 17, 19}, 20, 5};
+    struct Array arr2 = {{1, 6, 8, 17, 20}, 20, 5};
+    struct Array arr3 = {{}, arr1.size + arr2.size, 0};
+
+    Union(&arr1, &arr2, &arr3);
+    DisplayNum(arr3);
+
+    return 0;
+}
