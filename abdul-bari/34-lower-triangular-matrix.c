5d80f46d8fa156093a00b04493e428af09d3427a
Jagmeet Singh <jagmeetsingh2921@gmail.com>
2026-04-24 22:39:34 +0530

    Lower triangle matrix new formula

--- abdul-bari/34-lower-triangular-matrix.c
+++ abdul-bari/34-lower-triangular-matrix.c
@@ -6,13 +6,13 @@ struct Matrix {
 };
 
 void Set(struct Matrix *m, int i, int j, int x) {
-    if (i <= j)
-        m->A[ (((i -1) * m-> n) - (i-2) * (i-1)/2) + (j - i)] = x;
+    if (i >= j)
+        m->A[(i*(i-1)/2) + (j - 1)] = x;
 }
 
 int Get(struct Matrix m, int i, int j) {
-    if (i <= j)
-        return m.A[ (((i -1) * m.n) - (i-2) * (i-1)/2) + (j - i)];
+    if (i >= j)
+        return m.A[(i*(i-1)/2) + (j - 1)];
     else
         return 0;
 }
@@ -20,8 +20,8 @@ int Get(struct Matrix m, int i, int j) {
 void Display(struct Matrix m) {
     for (int i = 1; i <= m.n; i++) {
         for (int j = 1; j <= m.n; j++) {
-            if (i <= j)
-                printf("%d ", m.A[ (((i -1) * m.n) - (i-2) * (i-1)/2) + (j - i)]);
+            if (i >= j)
+                printf("%d ", m.A[(i*(i-1)/2) + (j - 1)]);
             else
                 printf("0 ");
         }
