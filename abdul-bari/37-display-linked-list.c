4f6719696f54075ea4ffc7463b7d910e93c9ab3b
Jagmeet Singh <jagmeetsingh2921@gmail.com>
2026-05-19 16:58:51 +0530

    linked list create and display

---
+++ abdul-bari/37-display-linked-list.c
@@ -0,0 +1,44 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+struct Node {
+    int data;
+    struct Node *next;
+} *first=NULL;
+
+void create(int A[], int n ){
+    int i ;
+    struct Node *t, *last;
+    first = (struct Node *)malloc(sizeof(struct Node));
+    first->data = A[0];
+    first->next = NULL;
+
+    last = first;
+
+    for(i=1; i < n; i++){
+            t = (struct Node *)malloc(sizeof(struct Node));
+            t->data = A[i];
+            t->next = NULL;
+
+            last -> next = t;
+            last = t;
+    }
+}
+
+void display(struct Node *p)
+{
+    while(p != NULL){
+        printf("%d\n", p -> data);
+        p=p->next;
+    }
+}
+
+int main()
+{
+    int A[] = { 3, 5, 7, 8, 9, 10};
+    create(A, 6);
+    display(first);
+
+
+    return 0;
+}
